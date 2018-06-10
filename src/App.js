df73bbb2d128a2aa479eb289c53b80d392c189bd
leekeunhwan <dlrmsghks7@gmail.com>
2018-06-10 18:42:26 +0900

    netlify http관련 이슈 해결

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
     );
 
     const data = await api_call.json();
@@ -49,31 +49,53 @@ class App extends React.Component {
   };
 
   render() {
-    return (
-      <div>
-        <div className="wrapper">
-          <div className="main">
-            <div className="container">
-              <div className="row">
-                <div className="col-xs-5 title-container">
-                  <Titles />
-                </div>
-                <div className="col-xs-7 form-container">
-                  <Form getWeather={this.getWeather} />
-                  <Weather
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    return ( <
+      div >
+      <
+      div className = "wrapper" >
+      <
+      div className = "main" >
+      <
+      div className = "container" >
+      <
+      div className = "row" >
+      <
+      div className = "col-xs-5 title-container" >
+      <
+      Titles / >
+      <
+      /div> <
+      div className = "col-xs-7 form-container" >
+      <
+      Form getWeather = {
+        this.getWeather
+      }
+      /> <
+      Weather temperature = {
+        this.state.temperature
+      }
+      city = {
+        this.state.city
+      }
+      country = {
+        this.state.country
+      }
+      humidity = {
+        this.state.humidity
+      }
+      description = {
+        this.state.description
+      }
+      error = {
+        this.state.error
+      }
+      /> < /
+      div > <
+      /div> < /
+      div > <
+      /div> < /
+      div > <
+      /div>
     );
   }
 }
