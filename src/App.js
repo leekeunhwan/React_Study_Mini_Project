81eb4510471130ca2b3e1e27e0468cee716a7d49
leekeunhwan <dlrmsghks7@gmail.com>
2018-06-10 18:58:20 +0900

    Readme.md 수정

--- src/App.js
+++ src/App.js
@@ -49,53 +49,31 @@ class App extends React.Component {
   };
 
   render() {
-    return ( <
-      div >
-      <
-      div className = "wrapper" >
-      <
-      div className = "main" >
-      <
-      div className = "container" >
-      <
-      div className = "row" >
-      <
-      div className = "col-xs-5 title-container" >
-      <
-      Titles / >
-      <
-      /div> <
-      div className = "col-xs-7 form-container" >
-      <
-      Form getWeather = {
-        this.getWeather
-      }
-      /> <
-      Weather temperature = {
-        this.state.temperature
-      }
-      city = {
-        this.state.city
-      }
-      country = {
-        this.state.country
-      }
-      humidity = {
-        this.state.humidity
-      }
-      description = {
-        this.state.description
-      }
-      error = {
-        this.state.error
-      }
-      /> < /
-      div > <
-      /div> < /
-      div > <
-      /div> < /
-      div > <
-      /div>
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
